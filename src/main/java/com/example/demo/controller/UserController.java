8595eaf346040f49a5a57a9754df93fa6a9a3d96
Alekearl <alexandrhurko@gmail.com>
2021-03-17 18:56:56 +0200

    checkstyle commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.demo.model.User;
 import com.example.demo.model.dto.UserDtoRequest;
 import com.example.demo.service.UserService;
 import com.example.demo.service.mapper.UserMapper;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/users")
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -3,10 +3,9 @@ package com.example.demo.service.impl;
 import com.example.demo.model.User;
 import com.example.demo.repositiry.UserRepository;
 import com.example.demo.service.UserService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
